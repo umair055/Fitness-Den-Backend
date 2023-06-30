7e69fdb79c3f7fdf569d102e4f74ae41b45a3ffe
umair055 <umairtariq403@gmail.com>
2023-06-30 22:17:20 +0500

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors((req, callback) => {
-    const allowedOrigins = ["http://localhost:3000"];
+    const allowedOrigins = ["https://fitnessden1.netlify.app"];
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
